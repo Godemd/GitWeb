46166fc57a3556d2bf112039e83c3bb1093bbdbf
Godemd <dandigrom1@gmail.com>
2024-09-27 12:25:34 +0300

    commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from catalog import views
+
+from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+    path("", views.home, name="home"),
+    path("catalog/", views.catalog, name="catalog"),
+    path("catalog/add/", views.add_product, name="catalog_add"),
+    path("catalog/<int:pk>/", views.product_detal, name="product"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
