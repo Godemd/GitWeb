be01278966f875e0742e8c8d24af0eada2a88f49
Godemd <dandigrom1@gmail.com>
2024-11-02 16:14:40 +0300

    Кэш

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Cashe
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
